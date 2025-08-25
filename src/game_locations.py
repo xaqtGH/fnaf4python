37907b6102f01ede340eefe570ed4fd9ddb95137
xaqtGH <your@mom.com>
2025-08-25 15:10:40 -0400

    new files

---
+++ src/game_locations.py
@@ -0,0 +1,2 @@
+# Setting up location-based flags used by the animatronics and the player
+# as well as defining the internal map logic
---
+++ src/game_mo.py
@@ -0,0 +1,7 @@
+# Movement opportunities are what allows most animatronics to move.
+# They vary from one animatronic to another, but the concept largely stays the same:
+# it affects Bonnie, Foxy, Chica, and Fredbear the most based on a timer
+# AI Level determines the frequency of these movement opportunities
+
+import random, math
+
