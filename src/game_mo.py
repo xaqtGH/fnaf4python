3cefc818518e0501b482b4d18957d111af842790
xaqtGH <your@mom.com>
2025-08-25 15:23:51 -0400

    game_mo

--- src/game_mo.py
+++ src/game_mo.py
@@ -3,5 +3,13 @@
 # it affects Bonnie, Foxy, Chica, and Fredbear the most based on a timer
 # AI Level determines the frequency of these movement opportunities
 
-import random, math
+import random, math, time
 
+def MovementOpportunity(animatronic):
+    match MovementOpportunity(animatronic):
+        case MovementOpportunity(Bonnie):
+            time.sleep(3)
+            BonnieMORoll = random.randint(BonnieAI, 20)
+            
+            if InFarLeftHall == True:
+                
